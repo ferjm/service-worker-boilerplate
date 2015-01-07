dc6b461579b9b8d8673c05c73d3867213d0671f9
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2015-01-07 11:15:53 +0100

    More cleanup

--- app/js/bootstrap.js
+++ app/js/bootstrap.js
@@ -1,12 +1,5 @@
 'use strict';
 
 addEventListener('load', function onLoad(e) {
-  window.serviceAPI = new ServiceAPI(function() {
-    // When index.html is loaded direclty, make sure it is controlled.
-    // so updates applies smoothly.
-    if (!navigator.serviceWorker.controller) {
-      debug("need soft reload");
-      doSoftReload();
-    }
-  });
+  window.serviceAPI = new ServiceAPI();
 });
